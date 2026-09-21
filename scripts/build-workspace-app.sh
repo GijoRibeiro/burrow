11299cfcd8c3dbec0347db95fd0f7d2e7af25da7
Gijo Ribeiro <18212185+GijoRibeiro@users.noreply.github.com>
2026-09-21 13:44:27 +0200

    Style messages awaiting agent acknowledgement with a dashed outline

--- scripts/build-workspace-app.sh
+++ scripts/build-workspace-app.sh
@@ -30,8 +30,8 @@ cat > "$bundle/Contents/Info.plist" <<'PLIST'
 <key>CFBundleName</key><string>Cloovies</string>
 <key>CFBundleIconFile</key><string>AppIcon</string>
 <key>CFBundleIdentifier</key><string>com.cloovies.workspace</string>
-<key>CFBundleVersion</key><string>30</string>
-<key>CFBundleShortVersionString</key><string>0.6.18</string>
+<key>CFBundleVersion</key><string>31</string>
+<key>CFBundleShortVersionString</key><string>0.6.19</string>
 <key>CFBundlePackageType</key><string>APPL</string>
 <key>NSHighResolutionCapable</key><true/>
 <key>LSMinimumSystemVersion</key><string>13.0</string>
